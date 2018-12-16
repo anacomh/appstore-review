60a2252ae9897604dcc3dcf1aa1c3109aad19e83
anacomh <anacomh@gmail.com>
2018-12-16 21:08:19 -0200

    removed tests comments

--- api/functions.php
+++ api/functions.php
@@ -19,8 +19,6 @@
     };
 
 /* JSON FUNCTION */
-// https://itunes.apple.com/br/app/bradesco/id336954985?mt=8
-// https://itunes.apple.com/br/rss/customerreviews/id=336954985/sortBy=mostRecent/page=3/json
 
 	function getjson( $appid,$page) {
 		if(!file_get_contents('https://itunes.apple.com/br/rss/customerreviews/page='.$page.'/id=' . $appid . '/sortby=mostrecent/json')) {
